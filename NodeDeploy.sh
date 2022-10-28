34b0f6e2bd0e442ef29c93ccfa060ba09050944d
Ankan Saha <93839804+AnkanSaha@users.noreply.github.com>
2022-10-28 12:27:07 +0530

    Removed Some scripts

--- NodeDeploy.sh
+++ NodeDeploy.sh
@@ -54,5 +54,3 @@ sudo apt install certbot python3-certbot-nginx
 # create nginx configuration
 sudo systemctl enable nginx
 sudo systemctl restart nginx
-sudo touch /etc/nginx/sites-available/network
-sudo su

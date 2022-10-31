ddda05c28304ce74f781ee3f7e532a411c7048d3
Ankan Saha <93839804+AnkanSaha@users.noreply.github.com>
2022-10-31 19:58:58 +0530

    Added Pm2 Configuration code

---
+++ pm2.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  apps : [{
+    name   : "AllSave",
+    script : "./Video-Downloader/downloader.js",
+    watch: true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+      "NODE_ENV": "production"
+    }
+  },
+  {
+    name   : "SaveNet",
+    script : "./SaveNet/data.js",
+    watch: true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+      "NODE_ENV": "production"
+    }
+  },
+  {
+    name   : "Portfolio",
+    script : "./Portfolio/app.js",
+    watch: true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+      "NODE_ENV": "production"
+    }
+  },
+  {
+    name   : "University Result",
+    script : "./University-Result/Result.js",
+    watch: true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+      "NODE_ENV": "production"
+    }
+  }
+  ]
+}

4ed326d4efffb05f0d851667d2953e51f81407b5
Ankan Saha <93839804+AnkanSaha@users.noreply.github.com>
2022-11-19 21:36:04 +0530

    Updated

--- auto_Updater.py
+++ auto_Updater.py
@@ -2,9 +2,9 @@ import os
 import time as timer
 
 # Path: auto_Updater.py
-time = 3600 # 10 days
+time = 3600 # 24 hours
 def Node_update():
-    os.system('sudo npm install -g pm2@latest');
+    os.system('sudo npm install -g pm2');
     os.system('sudo pm2 update');
     os.system('sudo npm install -g npm@latest');
     os.system('sudo npm install -g express@@latest');
@@ -15,25 +15,9 @@ def Node_update():
     os.system('sudo npm install -g cookie-parser@latest');
     os.system('sudo npm install -g express-validator@latest');
     os.system('sudo npm install -g express-flash@latest');
-    os.system('sudo npm install -g passport@latest');
-    os.system('sudo npm install -g passport-local@latest');
-    os.system('sudo npm install -g passport-http@latest');
-    os.system('sudo npm install -g passport-http-bearer@latest');
-    os.system('sudo npm install -g passport-jwt@latest');
     os.system('sudo npm install -g jsonwebtoken@latest');
     os.system('sudo npm install -g bcryptjs@latest');
     os.system('sudo npm install -g nodemailer@latest');
-    os.system('sudo npm install -g nodemailer-smtp-transport@latest');
-    os.system('sudo npm install -g nodemailer-sendgrid-transport@latest');
-    os.system('sudo npm install -g nodemailer-mailgun-transport@latest');
-    os.system('sudo npm install -g nodemailer-ses-transport@latest');
-    os.system('sudo npm install -g nodemailer-direct-transport@latest');
-    os.system('sudo npm install -g nodemailer-stub-transport@latest');
-    os.system('sudo npm install -g nodemailer-smtp-pool@latest');
-    os.system('sudo npm install -g nodemailer-wellknown@latest');
-    os.system('sudo npm install -g nodemailer-mandrill-transport@latest');
-    os.system('sudo npm install -g nodemailer-sparkpost-transport@latest');
-    os.system('sudo npm install -g nodemailer-postmark-transport@latest');
     os.system('sudo npm install -g cors@latest');
     os.system('sudo npm install -g multer@latest');
     os.system('sudo npm install -g multer-s3@latest');
@@ -47,6 +31,6 @@ while True:
     time = time - 1
     if time == 0:
         Node_update()
-        time = 3600 # 10 days
+        time = 3600 # 24 hours
     else:
         print('Waiting for Update in ' + str(time) + ' Seconds')

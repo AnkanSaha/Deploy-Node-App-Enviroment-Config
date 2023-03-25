378c10325e031a49f13932afb8253e8c365a0ef0
AnkanSaha <ankansahadaluabari@gmail.com>
2023-03-25 11:18:37 +0530

    Added: #567 Added Node Deployment Enviroment Shell Script For Ubuntu 22.04

--- Ubuntu 22.04 Deploy.sh
+++ Ubuntu 22.04 Deploy.sh
@@ -1,4 +1,4 @@
-# Ubuntu 20.04 LTS (Focal Fossa)
+# Ubuntu 22.04 LTS (Jammy)
 cd ..
 sudo apt install -y git # git
 sudo apt install -y nginx # nginx
@@ -6,32 +6,30 @@ sudo apt install -y curl # curl
 sudo apt install -y python3 # python3
 sudo apt install -y python # python2
 sudo apt install -y python3-pip # pip3
-# installing MongoDB for Ubuntu 20.04 LTS
-wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -   # add key
+# installing MongoDB for Ubuntu 22.04 LTS
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add - # add key
 sudo apt-get install gnupg # gnupg
-wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -  # add key
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add - # add key
 sudo touch /etc/apt/sources.list.d/mongodb-org-6.0.list # create file
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list # add repo
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list # add repo
 sudo apt-get update # update
 sudo apt-get install -y mongodb-org # install mongodb
-sudo systemctl start mongod # start mongodb
-sudo systemctl daemon- reload # reload
+sudo service mongod start # start mongodb
+sudo service mongod status # check status
+sudo service mongod restart # reload
 sudo systemctl enable mongod # enable mongodb
 sudo systemctl restart mongod # restart mongodb
 # installing NodeJS 19 for Ubuntu 20.04 LTS
 curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
-sudo apt-get install -y nodejs
-# installing yarn for Ubuntu 20.04 LTS
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - &&\
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list &&\
-sudo apt-get update && sudo apt-get install yarn
+sudo apt-get install -y nodejs # install nodejs
 # install npm
 sudo npm install -g npm@latest
+# installing yarn for Ubuntu 20.04 LTS
+npm install --global yarn # install yarn
+yarn --version # check version
 # install pm2
 sudo npm install -g pm2
-# import a file single file from github
 sudo pm2 update
-
 #ufw configuration
 sudo ufw enable
 sudo ufw allow 80/tcp

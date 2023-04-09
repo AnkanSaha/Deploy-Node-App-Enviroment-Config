d8de38db2255d8b03ea2d3c69f90d5911eb0794c
Ankan Saha <ankansahadaluabari@gmail.com>
2023-04-10 00:31:53 +0530

    Added : 571 Added Route Fore Store Manager

--- Ubuntu 20.04 Node Enviroment Setup.sh
+++ Ubuntu 20.04 Node Enviroment Setup.sh
@@ -46,6 +46,7 @@ sudo apt install certbot python3-certbot-nginx
 # create nginx configuration
 cd Node-Deployment-Configuration
 pm2 start auto_Updater.py
+sudo rm -r /etc/nginx/sites-available/default && sudo rm -r /etc/nginx/sites-enabled/default
 sudo cp NginxProxyRouter /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/NginxProxyRouter /etc/nginx/sites-enabled/
 sudo nginx -t

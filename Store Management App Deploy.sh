c64b67130bc52dea7161a41230f5d4a6905603ad
Ankan Saha <ankansahadaluabari@gmail.com>
2023-04-11 01:34:51 +0530

    Added #572 Added New Store Management App Deploy Script

---
+++ Store Management App Deploy.sh
@@ -0,0 +1,39 @@
+# Let's Import the Frontend App from Github
+cd ../ # Go to the parent folder
+git clone https://github.com/AnkanSaha/Store-Management-Frontend.git
+cd Store-Management-Frontend/ # Go to Frontend Folder
+
+# Let's build our Production ready version of the app
+npm install # Install all the dependencies
+npm run build # Build the Frontend App
+
+# import the Backend App from Github
+cd ../ # Go to the parent folder
+git clone https://github.com/AnkanSaha/Store-Management-Backend.git
+
+# Let's Build our Production ready version of Backend App
+cd ../Store-Management-Backend # Go to Backend Folder
+npm run build # Build the Backend App
+
+# Lets copy the build folder to the backend folder
+cd ../Store-Management-Frontend # Go to Frontend Folder
+
+# copy the build folder to the backend folder
+sudo mv ./dist ../Store-Management-Backend/Build/public
+
+
+# Create Enviromental variables File for the Backend App
+cd ../Store-Management-Backend/Build # Go to Backend Folder
+touch .env # Create the .env file
+sudo chmod +rwx .env # Give the .env file the permission to read and write
+
+# Add the enviromental variables to the .env file
+read -p "Enter PORT Number (only value) To Run Backend APP: " PORT # Ask the user to enter the PORT
+echo "STOREMANAGEMENTBACKENDPORT=$PORT" >> .env # Add the PORT to the .env file
+
+read -p "Enter the MongoDB Database URL (only value): " DB_URL # Ask the user to enter the DB_URL
+echo "STOREMANAGEMENTBACKENDMONGOURL=$DB_URL" >> .env # Add the DB_URL to the .env file
+
+# Let's start the Backend App and serve the Frontend App
+cd ../Store-Management-Backend/Build # Go to Backend Folder
+npm run start # Start the Backend App

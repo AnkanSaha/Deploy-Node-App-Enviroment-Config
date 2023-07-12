59ddaa297871cc15eff409297d62e217f5504f95
Ankan Saha <ankansahadaluabari@gmail.com>
2023-07-12 19:48:16 +0530

    Fixed

--- Service/Store Management App Deploy.sh
+++ Service/Store Management App Deploy.sh
@@ -35,8 +35,9 @@ else
     sudo mkdir $DirectoryPATH # Create Directory
     echo "Directory created." # Directory Created Message
 fi
+
 # Move Frontend to /var/www/html/StoreManagement
-sudo mv ./dist/* $DirectoryPATH # Move the Frontend App to the Directory
+sudo mv $DirectoryName/* $DirectoryPATH # Move the Frontend App to the Directory
 
 
 # Create Enviromental variables File for the Backend App
@@ -46,16 +47,16 @@ sudo chmod +rwx .env # Give the .env file the permission to read and write
 
 # Add the enviromental variables to the .env file
 read -p "Enter PORT Number (only value) To Run Backend APP: " PORT # Ask the user to enter the PORT
-echo "STOREMANAGEMENTBACKENDPORT=$PORT" >> .env # Add the PORT to the .env file
+echo "STORE_MANAGEMENT_BACKEND_PORT=$PORT" >> .env # Add the PORT to the .env file
 
 read -p "Enter the MongoDB Database URL (only value): " DB_URL # Ask the user to enter the DB_URL
-echo "STOREMANAGEMENTBACKENDMONGOURL=$DB_URL" >> .env # Add the DB_URL to the .env file
+echo "STORE_MANAGEMENT_BACKEND_MONGOURL=$DB_URL" >> .env # Add the DB_URL to the .env file
 
 read -p "Enter the LIVE URL for CORS: " LIVE_URL # Ask the user to enter the LIVE_URL
 echo "STORE_MANAGEMENT_LIVE_URL=$LIVE_URL" >> .env # Add the LIVE_URL to the .env file
 
 read -p "Enter JWT Secret Key : " JWT_SECRET_KEY # Ask the user to enter the JWT_SECRET_KEY
-echo"STORE_MANAGEMENT_JWT_SECRET=$JWT_SECRET_KEY" >> .env # Add the JWT_SECRET_KEY to the .env file
+echo "STORE_MANAGEMENT_JWT_SECRET=$JWT_SECRET_KEY" >> .env # Add the JWT_SECRET_KEY to the .env file
 
 # Let's start the Backend App and serve the Frontend App
 cd ../Store-Management-Backend/Build # Go to Backend Folder

b88ff6645b1f29c1b4d469a57c4da5c39bfd1d39
Ankan Saha <ankansahadaluabari@gmail.com>
2023-08-08 10:44:02 +0530

    Added Auto Delete if Already file exist

--- Service/Donation Tracker Deploy.sh
+++ Service/Donation Tracker Deploy.sh
@@ -5,6 +5,13 @@ cd ~ # Go to Home Directory
 ServiceFolder="Donation-Tracker" # Github Project Name
 GithubLink="https://github.com/AnkanSaha/Donation-Tracker.git"
 
+# Check if the Service Folder Exists
+if [ -d "$ServiceFolder" ]; then
+  # Delete the Service Folder
+  sudo rm -rf "$ServiceFolder"
+  echo "$ServiceFolder directory deleted."
+fi
+
 git clone  $GithubLink # Clone the Repository
 cd $ServiceFolder # Go to the Repository
 

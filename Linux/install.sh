66d719ad28c42ca60d6bd2a2eedbd18c2d1eeffc
Ankan Saha <ankansahadaluabari@gmail.com>
2023-10-30 18:41:40 +0530

    Added : Added 23.04 Script

--- Linux/install.sh
+++ Linux/install.sh
@@ -3,6 +3,7 @@
 # Import Ubuntu Dependencies Script
 source ./Ubuntu/Ubuntu_22.04_Node_Enviroment_Setup.sh # Import the script file
 source ./Ubuntu/Ubuntu_20.04_Node_Enviroment_Setup.sh # Import the script file
+source ./Ubuntu/Ubuntu_23.04_Node_Enviroment_Setup.sh # Import the script file
 
 read -p "Which OS you are using? (1) Ubuntu (2) CentOS (3) Debian (4) Other OS: " os # Ask the user which OS they are using
 
@@ -11,7 +12,7 @@ if [ "$os" == "1" ]; then
 
     if [ "$ubuntu_version" == "1" ]; then
         echo "Installing Ubuntu 23.04 Dependencies" # If the user chooses Ubuntu 23.04, then run
-        Ubuntu_23.04_Node_Enviroment_Setup
+        Ubuntu23.04NodeEnviromentSetup
     elif [ "$ubuntu_version" == "2" ]; then
         echo "Installing Ubuntu 22.04 Dependencies" # If the user chooses Ubuntu 22.04, then run
         Ubuntu22.04NodeEnviromentSetup

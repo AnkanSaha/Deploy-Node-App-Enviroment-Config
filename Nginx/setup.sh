1456abf0cb621e5eb8ef8dc67ca1eccc56b1eea7
Ankan Saha <ankansahadaluabari@gmail.com>
2023-10-31 12:55:20 +0530

    Added : Added Colors

--- Nginx/setup.sh
+++ Nginx/setup.sh
@@ -1,36 +1,59 @@
 #!/bin/bash
 
+# Define the colors which will be used in the script
+if [ -t 1 ]; then
+    RED=$(tput setaf 1)
+    GREEN=$(tput setaf 2)
+    YELLOW=$(tput setaf 3)
+    BLUE=$(tput setaf 4)
+    GRAY=$(tput setaf 8)
+    SAFERED=$(tput setaf 9)
+    MAGENTA=$(tput setaf 5)
+    BOLD=$(tput bold)
+    NORMAL=$(tput sgr0)
+else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    GRAY=""
+    SAFERED=""
+    MAGENTA=""
+    BOLD=""
+    NORMAL=""
+fi
+
 # Define the directory path
 DIRECTORY_PATH="/etc/nginx/conf.d/"
 
 # Check if the directory is empty or not
 if [ -n "$(ls -A $DIRECTORY_PATH)" ]; then
-    echo "Directory is not empty."
+    echo "$RED Directory is not empty."
 
     # Ask User is user want to overwrite all nginx config file from /etc/nginx/conf.d/ or not
-    read -p "Do you went to overwrite all nginx config file from /etc/nginx/conf.d/ (y/n): " OVERWRITE # Get OVERWRITE
+    read -p "$YELLOW $BOLD Do you went to overwrite all nginx config file from /etc/nginx/conf.d/ (y/n): " OVERWRITE # Get OVERWRITE
 
     # Check if user want to overwrite all nginx config file from /etc/nginx/conf.d/ or not
     if [ "$OVERWRITE" == "y" ] || [ "$OVERWRITE" == "Y" ]; then
-        echo "Removing all nginx config file from /etc/nginx/conf.d/"
+        echo "$RED Removing all nginx config file from /etc/nginx/conf.d/"
 
         # Delete all nginx config file from /etc/nginx/conf.d/
         sudo rm -r /etc/nginx/conf.d/* # Remove all nginx config file from /etc/nginx/conf.d/
-        echo "All nginx config file from /etc/nginx/conf.d/ is removed."
+        echo "$GREEN All nginx config file from /etc/nginx/conf.d/ is removed."
 
         # Copy nginx config file project wise to /etc/nginx/sites-available/
         sudo cp Conf/* /etc/nginx/conf.d/ # Copy nginx config file project wise to /etc/nginx/conf.d/
 
-        echo "All nginx config file from Conf/ is copied to /etc/nginx/conf.d/"
+        echo "$YELLOW $BOLD All nginx config file from Conf/ is copied to /etc/nginx/conf.d/"
 
     else
-        echo "No need to remove all nginx config file from /etc/nginx/conf.d/"
+        echo "$BOLD $MAGENTA No need to remove all nginx config file from /etc/nginx/conf.d/"
     fi
 else
-    echo "Directory is empty."
+    echo "$GREEN Directory is empty."
     # Copy nginx config file project wise to /etc/nginx/sites-available/
     sudo cp Conf/* /etc/nginx/conf.d/ # Copy nginx config file project wise to /etc/nginx/conf.d/
-    echo "All Conf/ nginx config file is copied to /etc/nginx/conf.d/"
+    echo "$YELLOW $BOLD All Conf/ nginx config file is copied to /etc/nginx/conf.d/"
 fi
 
 # Start nginx
@@ -40,12 +63,12 @@ sudo systemctl restart nginx # Restart nginx
 
 ## issue ssl certificate
 #All Domain Name for SSL Issue
-read -p "You need SSL Certificate? (y/n): " SSL # Get SSL Certificate
+read -p "$YELLOW $BOLD You need SSL Certificate? (y/n): " SSL # Get SSL Certificate
 
 if [ "$SSL" = "y" ]; then
     sudo certbot --nginx --redirect # Issue SSL Certificate for all domain name
 else
-    echo "No SSL Certificate Issued"
+    echo "$RED No SSL Certificate Issued"
 fi
 
 # Enable Certbot Auto Renewal
@@ -53,4 +76,4 @@ sudo systemctl enable certbot.timer  # Enable Certbot Auto Renewal
 sudo systemctl restart certbot.timer # Start Certbot Auto Renewal
 
 # End of Script
-echo "Server Ready With Nginx Setup 😄 Happy Deployment 🇮🇳 🛕 Jay Hind 🛕 🇮🇳" # End of Script
+echo "$BOLD $GREEN Server Ready With Nginx Setup 😄 Happy Deployment 🇮🇳 🛕 Jay Hind 🛕 🇮🇳" # End of Script

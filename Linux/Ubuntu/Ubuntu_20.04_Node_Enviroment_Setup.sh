97173edbed55a7a8b123c96ffc72edc3a5ad8fe4
Ankan Saha <ankansahadaluabari@gmail.com>
2024-01-13 19:05:49 +0530

    Upgraded: Upgraded Node JS from 20 to 21

--- Linux/Ubuntu/Ubuntu_20.04_Node_Enviroment_Setup.sh
+++ Linux/Ubuntu/Ubuntu_20.04_Node_Enviroment_Setup.sh
@@ -75,7 +75,7 @@ Ubuntu20.04NodeEnviromentSetup() {
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
-    NODE_MAJOR=20 # set node major version
+    NODE_MAJOR=21 # set node major version
     echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
     sudo apt-get update            # update apt
--- Linux/Ubuntu/Ubuntu_22.04_Node_Enviroment_Setup.sh
+++ Linux/Ubuntu/Ubuntu_22.04_Node_Enviroment_Setup.sh
@@ -69,13 +69,13 @@ Ubuntu22.04NodeEnviromentSetup() {
     sudo apt install -y python2     # python2
     sudo apt install -y python2-pip # python2-pip
 
-    # installing NodeJS 20 for Ubuntu 20.04 LTS
+    # installing NodeJS 20 for Ubuntu 22.04 LTS
     sudo apt-get update
     sudo apt-get install -y ca-certificates curl gnupg
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
-    NODE_MAJOR=20 # set node major version
+    NODE_MAJOR=21 # set node major version
     echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
     sudo apt-get update            # update apt
